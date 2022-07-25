9b3390f2fe1f1ec8a26f325ee3af7d2ae9f74d09
kimsanhaa <ttksgk@gmail.com>
2022-07-25 18:21:10 +0900

    최신

---
+++ src/main/java/life/semantics/kimsanha/dao/MapDAO.java
@@ -0,0 +1,19 @@
+package life.semantics.kimsanha.dao;
+
+import life.semantics.kimsanha.vo.MapVo;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface MapDAO {
+    void insertLocation(String locationName,String location, String phoneNum, String coordinate) throws DataAccessException;
+    List<MapVo> findTop10LocationList() throws DataAccessException; //10개만 select
+    void deleteLocation(String locationName) throws DataAccessException; //ㅇㅇ
+    List<MapVo> findLocationList() throws DataAccessException;
+    int countLocationList(String locationName) throws  DataAccessException;
+    int findLocationNum(String locationName)throws DataAccessException;
+}
